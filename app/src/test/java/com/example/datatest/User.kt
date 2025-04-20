54dc58f26e8698e81dfce1a308a2db0ba2a07380
TowyPrime <angeldti12@gmail.com>
2025-04-20 15:25:28 -0600

    Create User.kt

---
+++ app/src/test/java/com/example/datatest/User.kt
@@ -0,0 +1,12 @@
+//Crear tabla usuarios en la base de datos
+package com.example.datatest
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val username: String,
+    val password: String
+)
