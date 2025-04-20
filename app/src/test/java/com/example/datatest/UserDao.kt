ae077ce0d4af06652f6b6c2953fad876a31330ef
TowyPrime <angeldti12@gmail.com>
2025-04-20 15:26:27 -0600

    Create UserDao.kt

---
+++ app/src/test/java/com/example/datatest/UserDao.kt
@@ -0,0 +1,16 @@
+//Peticiones a la base de datos
+package com.example.datatest
+import androidx.room.*
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(user: User)
+
+    @Query("SELECT * FROM users WHERE username = :username AND password = :password")
+    suspend fun login(username: String, password: String): User?
+
+    @Query("SELECT * FROM users WHERE username = :username")
+    suspend fun getUserByUsername(username: String): User?
+}
